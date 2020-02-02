1116cdf55a0286c3d73b68b9676fe4e37ac9616d
Bastien <bchevriaut@gmail.com>
2020-02-02 17:05:57 +0100

    landing component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { MeComponent } from './components/me/me.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomeComponent,
     FooterComponent,
     NavbarComponent,
+    LandingComponent,
+    MeComponent,
   ],
   imports: [
     BrowserModule,
