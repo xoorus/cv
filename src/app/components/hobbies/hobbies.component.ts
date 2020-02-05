2da3fd5d0ec013785f73d081d1f4e414b23c0207
Bastien <bchevriaut@gmail.com>
2020-02-05 21:45:36 +0100

    ending hobbies component

--- src/app/components/hobbies/hobbies.component.ts
+++ src/app/components/hobbies/hobbies.component.ts
@@ -13,14 +13,45 @@ export class HobbiesComponent implements OnInit {
 
   constructor(private hobbyService: HobbyService) { }
 
-  hobbyList: Hobby[];
+  fullhobbyList: Hobby[];
+  hobbyListLeft: Hobby[];
+  hobbyListRight: Hobby[];
 
   ngOnInit() {
     this.getHobbies();
+
+    this.hobbyListLeft = [];
+    this.hobbyListRight = [];
+
+    this.fullhobbyList.forEach(hobby => {
+      hobby.id % 2 ? this.hobbyListLeft.push(hobby) : this.hobbyListRight.push(hobby);
+    });
+
+    this.hobbyListLeft = this.shuffle(this.hobbyListLeft);
+    this.hobbyListRight = this.shuffle(this.hobbyListRight);
   }
   
   getHobbies(): void {
     this.hobbyService.getHobbies()
-    .subscribe(hobbyList => this.hobbyList = hobbyList);
+    .subscribe(fullhobbyList => this.fullhobbyList = fullhobbyList);
+  }
+
+ shuffle(hobbies): Array<Hobby> {
+    var currentIndex = hobbies.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = hobbies[currentIndex];
+      hobbies[currentIndex] = hobbies[randomIndex];
+      hobbies[randomIndex] = temporaryValue;
+    }
+
+    return hobbies;
   }
 }
--- src/mock-hobbies.ts
+++ src/mock-hobbies.ts
@@ -1,10 +1,11 @@
 import { Hobby } from './objects/hobby';
 
 export const HOBBIES: Hobby[] = [
-  { id: 1, name: 'Voyages', icon: 'fa-globe',  desc: "best of bests" },
-  { id: 2, name: 'Sport', icon: 'fa-globe',  desc: "Escalade, rugby, un peu de course à pieds et de vélo" },
-  { id: 3, name: 'Jeux vidéos', icon: 'fa-globe',  desc: "Of course" },
-  { id: 4, name: 'Musique', icon: 'fa-globe',  desc: "Guitare, Piano" },
-  { id: 5, name: 'Informatique', icon: 'fa-globe',  desc: "C'est mon job" },
-  { id: 6, name: 'Montagne', icon: 'fa-globe',  desc: "Randos, escalade, ski" },
+  { id: 1, name: 'Voyages', icon: 'fa-plane',  desc: "J'ai eu la chance de pouvoir voyager sur presque tous les continents. <br>Mon voyage type ? => en sac à dos, à la rencontre des locaux. <br> J'aime les voyages qui sortent du commun et par dessus tout, j'adore discuter avec des personnes d'autres horizons.<br> C'est comme cela que je me suis retrouvé à donner des cours d'anglais pendant 1 mois et demi dans une école au centre du népal." },
+  { id: 2, name: 'Musique', icon: 'fa-music',  desc: "J'ai toujours beaucoup apprécié la musique. Je joue de la guitare et je suis entrain de me mettre (difficilement) au piano." },
+  { id: 3, name: 'Montagne', icon: 'fa-globe',  desc: "Randonnées, escalade, ski, paysages... Pour moi, la montagne est un magnifique terrain de jeu" },
+  { id: 4, name: 'Informatique', icon: 'fa-desktop',  desc: "J'ai la chance de travailler dans un domaine qui me passionne. L'évolution constante et fulgurante de l'informatique fait que c'est un métier qui change constamment. Je n'aime pas rester sur des acquis et l'informatique me le rend bien." },
+  { id: 5, name: 'Sport', icon: 'fa-futbol-o',  desc: "J'ai toujours aimé et pratiqué le sport. Escalade, Rugby, Judo, Natation, Boxe... J'ai toujours essayé d'avoir une activité en plus de mon travail.<br> C'est d'ailleurs cette passion qui m'a amené au Japon en octobre dernier pour suivre la coupe du monde de Rugby !" },
+  { id: 6, name: 'Jeux vidéos', icon: 'fa-gamepad',  desc: "Difficile de parler d'informatique sans parler de jeux vidéos. C'est la principale raison de mon orientation vers l'Informatique : Comment le jeu auquel je joue ne peux être qu'une suite de 1 et de 0." },
+  { id: 8, name: 'Photographie', icon: 'fa-camera',  desc: "J'ai découvert la photographie lors de mes voyages. Ce n'est pas un loisir que je pratique au jour le jour, cependant je ne pars jamais voyager sans mon appareil et j'ai toujours l'oeil dans l'objectif!" },
 ];
