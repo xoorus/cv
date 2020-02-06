3777b0edab8c10015cdc38b2f3f2cc0bf42b2e1e
Bastien <bchevriaut@gmail.com>
2020-02-06 21:18:41 +0100

    adding footer

--- src/mock-hobbies.ts
+++ src/mock-hobbies.ts
@@ -2,7 +2,7 @@ import { Hobby } from './objects/hobby';
 
 export const HOBBIES: Hobby[] = [
   { id: 1, name: 'Voyages', icon: 'fa-plane',  desc: "J'ai eu la chance de pouvoir voyager sur presque tous les continents. <br>Mon voyage type ? => en sac à dos, à la rencontre des locaux. <br> J'aime les voyages qui sortent du commun et par dessus tout, j'adore discuter avec des personnes d'autres horizons.<br> C'est comme cela que je me suis retrouvé à donner des cours d'anglais pendant 1 mois et demi dans une école au centre du népal." },
-  { id: 2, name: 'Musique', icon: 'fa-music',  desc: "J'ai toujours beaucoup apprécié la musique. Je joue de la guitare et je suis entrain de me mettre (difficilement) au piano." },
+  { id: 2, name: 'Musique', icon: 'fa-music',  desc: "J'ai toujours beaucoup apprécié la musique. Je joue de la guitare et je suis entrain d'apprendre, non sans difficultés, le piano." },
   { id: 3, name: 'Montagne', icon: 'fa-globe',  desc: "Randonnées, escalade, ski, paysages... Pour moi, la montagne est un magnifique terrain de jeu" },
   { id: 4, name: 'Informatique', icon: 'fa-desktop',  desc: "J'ai la chance de travailler dans un domaine qui me passionne. L'évolution constante et fulgurante de l'informatique fait que c'est un métier qui change constamment. Je n'aime pas rester sur des acquis et l'informatique me le rend bien." },
   { id: 5, name: 'Sport', icon: 'fa-futbol-o',  desc: "J'ai toujours aimé et pratiqué le sport. Escalade, Rugby, Judo, Natation, Boxe... J'ai toujours essayé d'avoir une activité en plus de mon travail.<br> C'est d'ailleurs cette passion qui m'a amené au Japon en octobre dernier pour suivre la coupe du monde de Rugby !" },
