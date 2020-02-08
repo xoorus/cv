b0ab245ca8b1874b8e6eed18e7fb9bb1af7d8694
Bastien <bchevriaut@gmail.com>
2020-02-08 17:08:00 +0100

    responsive almost finished

--- src/mock-education-list.ts
+++ src/mock-education-list.ts
@@ -1,8 +1,8 @@
 import { Education } from './objects/education';
 
 export const EDUCATION_LIST: Education[] = [
-  { id: 1, icon: 'fa-check-square-o', name: 'SugarCRM Administration Specialist', score:'', desc: 'Certification administrateur de la solution CRM SugarCRM', logo: 'assets/sugarcrm.png', location: 'Genève, Suisse', date: '07.2017', deliveredBy: 'SugarCRM'}, 
-  { id: 2, icon: 'fa-check-square-o', name: 'SugarCRM Developer Specialist', score:'', desc: 'Certification administrateur de la solution CRM SugarCRM', logo: 'assets/sugarcrm.png', location: 'Genève, Suisse', date: '07.2017', deliveredBy: 'SugarCRM'}, 
+  { id: 1, icon: 'fa-check-square-o', name: 'SugarCRM Admin. Specialist', score:'', desc: 'Certification administrateur de la solution CRM SugarCRM', logo: 'assets/sugarcrm.png', location: 'Genève, Suisse', date: '07.2017', deliveredBy: 'SugarCRM'}, 
+  { id: 2, icon: 'fa-check-square-o', name: 'SugarCRM Developper Specialist', score:'', desc: 'Certification administrateur de la solution CRM SugarCRM', logo: 'assets/sugarcrm.png', location: 'Genève, Suisse', date: '07.2017', deliveredBy: 'SugarCRM'}, 
   { id: 3, icon: 'fa-check-square-o', name: 'XperiDo Administrator', score:'', desc: 'Solution de production de documents automatisée', logo: 'assets/invenso.png', location: 'Genève, Suisse', date: '06.2017', deliveredBy: 'Invenso'}, 
   { id: 4, icon: 'fa-graduation-cap', name: 'BAC+5 Informatique', score:'', desc: "Bac + 5 réalisé à l'EPSI. Spécialités systèmes d'information et génie logiciel", logo: 'assets/epsi.png', location: 'Bordeaux, France', date: '07.2015', deliveredBy: 'EPSI'},
   { id: 5, icon: 'fa-globe', name: 'TOEIC', score:'815/990', desc: "Test d'anglais", logo: 'assets/toeic.png', location: 'Bordeaux, France', date: '06.2014', deliveredBy: 'TOEIC'},
